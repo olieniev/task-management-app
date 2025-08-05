854cd2dbb11563a135d3030ae400c0a3c3ad1c75
Yaroslav Olieniev <olieniev.od.ua@gmail.com>
2025-08-05 09:55:08 +0200

    initial commit

---
+++ src/test/java/com/olieniev/taskmanagement/TaskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olieniev.taskmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
